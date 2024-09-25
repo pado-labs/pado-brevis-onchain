41a67cce44dfdc3522525a1acbd567461eb3aee2
Dean Xu <xuda.it@outlook.com>
2024-09-25 11:03:50 +0800

    update docker script

--- app/docker-build.sh
+++ app/docker-build.sh
@@ -6,6 +6,7 @@ else
 fi
 npm install -g pkg
 npm install
+npm run build
 pkg . --output dist/pado-brevis-app
 docker build -t padolabs/pado-brevis-app:$DEFAULT_VALUE .
 #docker push padolabs/pado-brevis-:$DEFAULT_VALUE
